fbee9e1f9b31e9fef5abda1bc996e3f50a5bfd49
Dalton <DY@test.com>
2022-11-15 21:08:06 -0600

    Made changes to some in-class code, werent correct

--- webscraping-Bible2.py
+++ webscraping-Bible2.py
@@ -3,12 +3,14 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 from urllib.request import urlopen, Request
 
-chapters = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12',
-                   '13', '14', '15', '16', '17', '18', '19', '20', '21']
+random_chapter = random.randint(1,21)
 
-i = random.choice(chapters)
+if random_chapter < 0: 
+    random_chapter = "0" + str(random_chapter)
+else:
+    random_chapter = str(random_chapter)
 
-webpage = 'https://biblehub.com/asv/john/' + i + '.htm'
+webpage = 'https://biblehub.com/niv/john/' + random_chapter + '.htm'
 
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3'}
 
@@ -18,18 +20,18 @@ webpage = urlopen(req).read()
 
 soup = BeautifulSoup(webpage,'html.parser')
 
-page_verses = soup.findAll('div',class_='reg')
-print(page_verses)
-input()
+page_verses = soup.findAll('div',attrs={"class":"chap"})
+#print(page_verses)
+
 for verse in page_verses:
     verse_list = verse.text.split(".")
 
 
-myverse = random.choice(verse_list[:-5])
+myverse = random.choice(verse_list[:len(verse_list)-5])
 
 #print(f"Chapter: {i} , Verse: {myverse}")
 
-message = "Chapter: " + i + " Verse:" + myverse
+message = "Chapter: " + random_chapter + " Verse:" + myverse
 
 print(message)
 
